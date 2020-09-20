4236ff146e272e980a4d951eadd593a691d57f3a
T4toun3 <oettam32@gmail.com>
2020-09-20 14:21:06 +0200

    some test

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod string_utils;
 
 pub mod search;
 
-pub mod doujishi;
+pub mod doujin;
 
 pub mod table;
 
@@ -11,7 +11,7 @@ mod tests {
 
     use std::time::{SystemTime};
 
-    use crate::doujishi::Doujishi;
+    use crate::doujin::Doujin;
     use crate::search::{Search, SearchArgs};
     use crate::table::*;
 
@@ -20,14 +20,14 @@ mod tests {
         let time_start = SystemTime::now();
         /*let args = vec![SearchArgs::Page(8), SearchArgs::Page(9)];
         let url = Search::build_url_with_args(args);
-        println!("{:?}", &url);*/
-        /*let search = Doujishi::new(316932).unwrap(); // On fait une recherche sur cette url
-        println!("{:#?}", search.tags);*/
-        let table = TagTable::new().unwrap();
+        let search = Search::new(&url);*/
+        let search = Doujin::new(316932).unwrap(); // On fait une recherche sur cette url
+        println!("{:#?}", search.similars);
+        /*let table = TagTable::new().unwrap();
         let table = ArtistTable::new().unwrap();
         let table = CharacterTable::new().unwrap();
         let table = ParodieTable::new().unwrap();
-        let table = GroupTable::new().unwrap();
+        let table = GroupTable::new().unwrap();*/
         /*println!("{:?}", &table.get_by_id(14520));
         println!("{:?}", &table.get_by_name("ahegao"));
         println!("{:?}", &table.max());
@@ -42,11 +42,11 @@ mod tests {
         /*let rand = Search::random();
         println!("{:#?}", rand);*/
 
-        /*let page = crate::doujishi::Doujishi::new(search.entries[0].id).unwrap(); // On prend la première page de la recherche
+        /*let page = crate::doujin::Doujin::new(search.entries[0].id).unwrap(); // On prend la première page de la recherche
         println!("{:#?}",page.get_pages_image_urls()); // On récupère l'url de toutes les images du manga
         println!("{:#?}",Search::new("https://nhentai.net/search/?q=test&page=1&sort=popular-week"));*/
 
-        /*let doujin = Doujishi::new(327341).unwrap();
+        /*let doujin = Doujin::new(327341).unwrap();
         println!("{:#?}", doujin);*/
 
         /*println!("get_page_image_url_small : {}", doujin.get_page_image_url_small(1).unwrap());
