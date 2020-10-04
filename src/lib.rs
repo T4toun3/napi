9bb0eb700a6bbcb1a61f580f7092bb8595d9ebc3
T4toun3 <oettam32@gmail.com>
2020-10-04 13:37:09 +0200

    Some test

--- src/lib.rs
+++ src/lib.rs
@@ -9,36 +9,36 @@ pub mod table;
 #[cfg(test)]
 mod tests {
 
-    use std::time::{SystemTime};
+    use std::time::SystemTime;
 
     use crate::doujin::Doujin;
-    use crate::search::{Search, SearchArgs};
+    use crate::search::{Search, SearchArgs, Sort};
     use crate::table::*;
 
     #[test]
     fn it_works() {
         let time_start = SystemTime::now();
 
-        let table1 = LanguageTable::new().unwrap();
-        let table2 = LanguageTable::new_by_popularity().unwrap();
+        let mut search_pop = Search::search_populars(Sort::Popular).unwrap();
+        search_pop.merge_search_pages(0);
+        println!("All results:");
+        for result in &search_pop.entries {
+            println!("[{}] {}",result.id,result.name) // On affiche toutes les entrées trouvées
+        }
 
+        /*let table1 = LanguageTable::new().unwrap();
+        let table2 = LanguageTable::new_by_popularity().unwrap();
         let mut table1 = table1;
         table1.sort_by_popularity();
-
         println!("==== ARTIST_TABLE 1 ====\n{:#?}", table1.get(2));
-        println!("==== ARTIST_TABLE 2 ====\n{:?}", table2.max());
+        println!("==== ARTIST_TABLE 2 ====\n{:?}", table2.max());*/
 
-        /*let args = vec![SearchArgs::Page(8), SearchArgs::Page(9)];
-        let url = Search::build_url_with_args(args);
-        let search = Search::new(&url);*/
-        /*let search = Doujin::new(316932).unwrap(); // On fait une recherche sur cette url
-        println!("{:#?}", search.similars);*/
         /*let table = TagTable::new().unwrap();
         let table = ArtistTable::new().unwrap();
         let table = CharacterTable::new().unwrap();
         let table = ParodieTable::new().unwrap();
-        let table = GroupTable::new().unwrap();*/
-        /*println!("{:?}", &table.get_by_id(14520));
+        let table = GroupTable::new().unwrap();
+        println!("{:?}", &table.get_by_id(14520));
         println!("{:?}", &table.get_by_name("ahegao"));
         println!("{:?}", &table.max());
         println!("{:?}", &table.min());*/
@@ -51,8 +51,14 @@ mod tests {
 
         /*let rand = Search::random();
         println!("{:#?}", rand);*/
+        
+        /*let doujin = Doujin::new(316932).unwrap(); // On fait une recherche sur cette url
+        println!("{:#?}", doujin.similars);*/
 
-        /*let page = crate::doujin::Doujin::new(search.entries[0].id).unwrap(); // On prend la première page de la recherche
+        /*let args = vec![SearchArgs::Page(8), SearchArgs::Page(9)];
+        let url = Search::build_url_with_args(args);
+        let search = Search::new(&url).unwrap();
+        let page = crate::doujin::Doujin::new(search.entries[0].id).unwrap(); // On prend la première page de la recherche
         println!("{:#?}",page.get_pages_image_urls()); // On récupère l'url de toutes les images du manga
         println!("{:#?}",Search::new("https://nhentai.net/search/?q=test&page=1&sort=popular-week"));*/
 
