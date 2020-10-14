d523fa1f156c628a157fe92b0a4bda70aa86276b
T4toun3 <oettam32@gmail.com>
2020-10-14 20:02:12 +0200

    it's work ?

--- derive_macro/target/rls/debug/build/syn-6fef451f7ed71496/build_script_build-6fef451f7ed71496.d
+++
@@ -1,5 +0,0 @@
-d:\Programmation\GitHub\napi\derive_macro\target\rls\debug\build\syn-6fef451f7ed71496\build_script_build-6fef451f7ed71496.exe: C:\Users\T4toun3\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.43\build.rs
-
-d:\Programmation\GitHub\napi\derive_macro\target\rls\debug\build\syn-6fef451f7ed71496\build_script_build-6fef451f7ed71496.d: C:\Users\T4toun3\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.43\build.rs
-
-C:\Users\T4toun3\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.43\build.rs:
