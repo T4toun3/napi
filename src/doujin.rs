a258b9dae710159741711000cd96a7c7e82e2f20
Tatounee <oettam32@gmail.com>
2021-05-06 13:56:08 +0200

    Add some comments

--- src/doujin.rs
+++ src/doujin.rs
@@ -13,6 +13,7 @@ pub struct Doujin {
     pub id: u32,
     #[serde(deserialize_with = "string_to_u32")]
     pub media_id: u32,
+    // TODO: change this because it's all the time "english" + "japaness" + "pretty" (struct)
     pub title: HashMap<String, String>, // Lang - Title
     #[serde(deserialize_with = "unix_to_date")]
     pub upload_date: NaiveDateTime,
@@ -45,6 +46,22 @@ impl Doujin {
             .collect::<Vec<SearchEntry>>();
 
         Some(doujin)
+
+        // let text = html
+        //     .after("JSON.parse(\"")
+        //     .before("\");").unwrap().replace("\\u0022", "\"").replace("\\u002F", "/");
+
+        // let dunjin = &mut serde_json::Deserializer::from_str(&text);
+
+        // let result: Result<Doujin, _> = serde_path_to_error::deserialize(dunjin);
+        // match result {
+        //     Ok(_) => panic!("expected a type error"),
+        //     Err(err) => {
+        //         let path = err.into_inner().to_string();
+        //         assert_eq!(path, "dependencies.serde.version");
+        //     }
+        // }
+        // None
     }
 
     pub fn get_image_url_small(&self, page: u16) -> Option<String> {
@@ -245,3 +262,8 @@ pub struct ImageFormat {
     pub w: u16,
     pub h: u16,
 }
+
+// #[test]
+// fn deserd() {
+//     let data = r#"{"id":141506,"media_id":"844469","title":{"english":"(C88) [In The Sky (Nakano Sora)] Shuuya ni Omou (Kantai Collection -KanColle-) [Chinese] [\u005Cu5c4f\u005Cu5e55\u005Cu9ad2\u005Cu4e86\u005Cu6f22\u005Cu5316\u005Cu7d44]","japanese":"(C88) [In The Sky (\u005Cu4e2d\u005Cu4e43\u005Cu7a7a)] \u005Cu79cb\u005Cu591c\u005Cu30cb\u005Cu60f3\u005Cu30d5 (\u005Cu8266\u005Cu968a\u005Cu3053\u005Cu308c\u005Cu304f\u005Cu3057\u005Cu3087\u005Cu3093 -\u005Cu8266\u005Cu3053\u005Cu308c-) [\u005Cu4e2d\u005Cu56fd\u005Cu7ffb\u005Cu8a33]","pretty":"Shuuya ni Omou"},"images":{"pages":[{"t":"j","w":1200,"h":1689},{"t":"j","w":1200,"h":1689},{"t":"j","w":1200,"h":1693},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1735},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1751},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":847},{"t":"j","w":1200,"h":1716},{"t":"j","w":1200,"h":1756},{"t":"j","w":1200,"h":1761},{"t":"p","w":1024,"h":711}],"cover":{"t":"j","w":350,"h":493},"thumbnail":{"t":"j","w":250,"h":352}},"scanlator":"","upload_date":1440162796,"tags":[{"id":1841,"type":"parody","name":"kantai collection","url":"\u002Fparody\u002Fkantai-collection\u002F","count":13889},{"id":10248,"type":"artist","name":"nakano sora","url":"\u002Fartist\u002Fnakano-sora\u002F","count":180},{"id":10314,"type":"tag","name":"schoolgirl uniform","url":"\u002Ftag\u002Fschoolgirl-uniform\u002F","count":56614},{"id":13720,"type":"tag","name":"nakadashi","url":"\u002Ftag\u002Fnakadashi\u002F","count":43099},{"id":17249,"type":"language","name":"translated","url":"\u002Flanguage\u002Ftranslated\u002F","count":111427},{"id":17488,"type":"group","name":"in the sky","url":"\u002Fgroup\u002Fin-the-sky\u002F","count":125},{"id":20035,"type":"tag","name":"x-ray","url":"\u002Ftag\u002Fx-ray\u002F","count":21189},{"id":26980,"type":"character","name":"akizuki","url":"\u002Fcharacter\u002Fakizuki\u002F","count":90},{"id":29859,"type":"tag","name":"blowjob","url":"\u002Ftag\u002Fblowjob\u002F","count":37977},{"id":29922,"type":"character","name":"teitoku","url":"\u002Fcharacter\u002Fteitoku\u002F","count":7823},{"id":29963,"type":"language","name":"chinese","url":"\u002Flanguage\u002Fchinese\u002F","count":45212},{"id":33172,"type":"category","name":"doujinshi","url":"\u002Fcategory\u002Fdoujinshi\u002F","count":231184},{"id":35762,"type":"tag","name":"sole female","url":"\u002Ftag\u002Fsole-female\u002F","count":67502},{"id":35763,"type":"tag","name":"sole male","url":"\u002Ftag\u002Fsole-male\u002F","count":61245}],"num_pages":24,"num_favorites":424}"#;
+// }
